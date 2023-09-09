22ed99040534668bb0f3568f4b44fb0fa2054405
Shadi-Almohtaseb <shadi876543211@gmail.com>
2023-09-09 21:34:49 +0300

    added simple contact form

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -5,6 +5,32 @@ const Contact = () => {
   return (
     <div className="flex justify-between lg:flex-row flex-col lg:pr-28">
       <LeftBanner />
+      <div className="w-full mt-36">
+        <div className="flex items-center justify-center flex-col gap-8">
+          <input
+            type="text"
+            id="first_name"
+            class="bg-gray-100 border w-full max-w-[40rem] border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block outline-none p-2.5 "
+            placeholder="Full Name..."
+            required
+          />
+          <input
+            type="text"
+            id="first_name"
+            class="bg-gray-100 border w-full max-w-[40rem] border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block outline-none p-2.5 "
+            placeholder="Email..."
+            required
+          />
+          <textarea
+            name=""
+            id=""
+            cols="50"
+            rows="19"
+            placeholder="Message..."
+            className=" border-gray-300 bg-gray-100 border rounded-lg w-full max-w-[40rem] p-3"
+          ></textarea>
+        </div>
+      </div>
     </div>
   );
 };
