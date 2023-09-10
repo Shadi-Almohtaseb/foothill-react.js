03bae28c46a20874875858bd09d61f60d2cb354b
Shadi-Almohtaseb <shadi876543211@gmail.com>
2023-09-10 19:27:52 +0300

    finished projects card

--- portfolio/src/components/LeftCardProject.jsx
+++ portfolio/src/components/LeftCardProject.jsx
@@ -1,53 +1,41 @@
 import React from "react";
-import projectImage1 from "../images/Projects/tw5.png";
 
-const ProjectCard = () => {
+const ProjectCard = ({ item }) => {
   return (
-    <div className="flex items-center bg-[#4990EB] p-4 rounded-lg mt-5 shadow-lg">
-      <img src={projectImage1} alt="" className="w-[25rem] h-fit rounded-lg " />
-      <div className=" w-full">
-        <div className="flex items-center justify-between gap-9 ml-10">
-          <span className="text-[1.6rem] text-white">Twitter Clone</span>
-          <div className="flex items-center justify-between self  gap-7">
-            <img
-              width="28"
-              height="28"
-              src="https://img.icons8.com/glyph-neue/64/FFFFFF/github.png"
-              alt="github"
-            />
-            <img
-              width="28"
-              height="28"
-              src="https://img.icons8.com/material-sharp/24/FFFFFF/visible.png"
-              alt="visible"
-            />
+    <div className="flex items-center flex-col bg-[#4990EB] p-4 rounded-lg mt-5 w-fit shadow-lg min-h-[16rem]">
+      <div className="flex items-center 2xl:flex-row flex-col px-3">
+        <img src={item.image} alt="" className="w-[25rem] h-fit rounded-lg" />
+        <div className=" 2xl:w-full max-w-[25rem] mt-6 px-3">
+          <div className="flex items-center justify-between gap-9 2xl:ml-10">
+            <span className="text-[1.6rem] text-white">{item.title}</span>
+            <div className="flex items-center justify-between self  gap-7">
+              <img
+                width="28"
+                height="28"
+                src="https://img.icons8.com/glyph-neue/64/FFFFFF/github.png"
+                alt="github"
+              />
+              <img
+                width="28"
+                height="28"
+                src="https://img.icons8.com/material-sharp/24/FFFFFF/visible.png"
+                alt="visible"
+              />
+            </div>
+          </div>
+          <div className="2xl:ml-10 mt-4 text-white">
+            <p>{item.description}</p>
           </div>
         </div>
-        <div className="ml-10 mt-4 text-white">
-          <p>
-            A fully responsive Full stack application with Similar Twitter UI,
-            and Google authentication, CRUD operations for tweets, likes,
-            comments, likes on comments, personal pages, protected routes and
-            permissions, two general API's and much more with the powerful of
-            Firebase.
-          </p>
-        </div>
-        <div className="flex gap-3 flex-wrap ml-10 mt-10 text-white">
-          <span className="py-1 px-4 bg-[#ffffff2b] rounded-full">
-            Firebase
-          </span>
-          <span className="py-1 px-4 bg-[#ffffff2b] rounded-full">Next.js</span>
-
-          <span className="py-1 px-4 bg-[#ffffff2b] rounded-full">
-            Tailwind CSS
-          </span>
-          <span className="py-1 px-4 bg-[#ffffff2b] rounded-full">
-            Ant Design
-          </span>
-          <span className="py-1 px-4 bg-[#ffffff2b] rounded-full">
-            State Management (recoil)
-          </span>
-        </div>
+      </div>
+      <div className="flex gap-3 flex-wrap mt-5 max-w-[25rem] 2xl:max-w-full text-white">
+        {item.stack.map((item, index) => {
+          return (
+            <span key={index} className="py-1 px-4 bg-[#ffffff2b] rounded-full">
+              {item}
+            </span>
+          );
+        })}
       </div>
     </div>
   );
--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -10,20 +10,18 @@ const Contact = () => {
           <input
             type="text"
             id="first_name"
-            class="bg-gray-100 border w-full max-w-[40rem] border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block outline-none p-2.5 "
+            className="bg-gray-100 border w-full max-w-[40rem] border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block outline-none p-2.5 "
             placeholder="Full Name..."
             required
           />
           <input
             type="text"
             id="first_name"
-            class="bg-gray-100 border w-full max-w-[40rem] border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block outline-none p-2.5 "
+            className="bg-gray-100 border w-full max-w-[40rem] border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block outline-none p-2.5 "
             placeholder="Email..."
             required
           />
           <textarea
-            name=""
-            id=""
             cols="50"
             rows="19"
             placeholder="Message..."
