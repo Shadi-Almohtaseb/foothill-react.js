07683e218c68b93175f733fec89ee618d99ca605
Shadi-Almohtaseb <shadi876543211@gmail.com>
2023-09-11 21:56:22 +0300

    fixed contact page

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -3,10 +3,10 @@ import LeftBanner from "../components/LeftBanner";
 
 const Contact = () => {
   return (
-    <div className="flex justify-between lg:flex-row flex-col lg:pr-28">
+    <div className="flex justify-between lg:flex-row flex-col lg:pr-28 mb-12">
       <LeftBanner />
-      <div className="w-full mt-36">
-        <div className="flex items-center justify-center flex-col gap-8">
+      <div className="w-full flex justify-center mt-36">
+        <div className="flex items-center max-w-[40rem] w-full flex-col gap-8 mx-2">
           <input
             type="text"
             id="first_name"
@@ -25,8 +25,11 @@ const Contact = () => {
             cols="50"
             rows="19"
             placeholder="Message..."
-            className=" border-gray-300 bg-gray-100 border rounded-lg w-full max-w-[40rem] p-3"
+            className="bg-gray-100 border-gray-300 border text-gray-900 text-sm r focus:ring-blue-500 focus:border-blue-500 outline-none rounded-lg w-full max-w-[40rem] p-3"
           ></textarea>
+          <button className="px-10 py-2 ml-2 bg-[#4990EB] self-start text-white rounded-md font-normal shadow-lg">
+            Submit
+          </button>
         </div>
       </div>
     </div>
